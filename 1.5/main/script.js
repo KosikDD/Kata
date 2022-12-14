3de5c6afcbea5802e5c9ae1c0a809a80e5a24a8c
KosikDD <117274598+KosikDD@users.noreply.github.com>
2022-12-14 23:57:00 +0200

    Add files via upload

--- 1.5/main/script.js
+++ 1.5/main/script.js
@@ -1,17 +1,75 @@
 let readmore = document.querySelector('.read-more');
+let readmoreText = document.querySelector('.read-more-text');
+let arrow = document.querySelector('.arrow');
+let brand_elements = document.querySelectorAll('.brand');
+
+function hide(elements) {
+  for (element of elements){ 
+
+    if(!element.classList.contains('hidden')) {
+      element.classList.add('hidden');
+      event.target.textContent = 'Показать все';
+      event.currentTarget.classList.remove('arrow-reversed');
+      readmore.classList.add('arrow');
+    } else {
+      element.classList.remove('hidden');
+      event.target.textContent = 'Скрыть';
+      event.currentTarget.classList.add('arrow-reversed');
+      readmore.classList.remove('arrow');
+    }
+  }
+}
 
 readmore.addEventListener('click', function() {
-
-  let hidden = document.querySelectorAll('.hidden');
-
-  for (i of hidden){ 
-
-    if(i.style.display === 'flex') {
-      i.style.display = 'none';
-      this.textContent = 'Показать все';
-    } else {
-      i.style.display = 'flex';
-      this.textContent = 'Скрыть';
+  if (window.innerWidth >= 768 && window.innerWidth < 1120){
+    hide(brand_elements);
+    for (let i = 0; i <= 5; i++){
+      brand_elements[i].classList.remove('hidden');
+    } 
+  } else {
+    hide(brand_elements);
+    for (let i = 0; i <= 7; i++){
+      brand_elements[i].classList.remove('hidden');
     }
   }
 });
+
+
+
+function Hiddengroup() {
+  for (element of brand_elements){
+    element.classList.remove('hidden')
+  }
+  
+  if (window.innerWidth < 768){
+    for (let i = brand_elements.length - 1; i > 8; i--){
+      brand_elements[i].classList.add('hidden');
+    }
+    
+  } else if (window.innerWidth >= 768 && window.innerWidth < 1120){
+    for (let i = brand_elements.length - 1; i > 5; i--){
+      brand_elements[i].classList.add('hidden');
+    }
+
+  } else {
+    for (let i = brand_elements.length - 1; i > 7; i--){
+      brand_elements[i].classList.add('hidden');
+    }
+  }
+
+  if (readmore.classList.contains('arrow-reversed')) {
+    readmore.classList.remove('arrow-reversed');
+  }
+  readmore.classList.add('arrow');
+  readmoreText.textContent = 'Показать все';
+  
+}
+
+window.addEventListener('DOMContentLoaded',  () => {
+  Hiddengroup();
+});
+
+window.addEventListener('resize',  () => {
+  Hiddengroup();
+});
+
